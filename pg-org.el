57056a183b10c0b2fd3714d93548377d494a2a31
mathink <mathink07@gmail.com>
2014-12-04 21:54:36 +0900

    po:update-prev doesn't evaluate script.

--- pg-org.el
+++ pg-org.el
@@ -9,7 +9,7 @@
   (defvar po:update-point 1)
 
   (define-key pg-org-mode-map
-    (kbd "C-c C-<return>") 'po:update)
+    (kbd "C-c C-<return>") 'po:update-here)
   (define-key pg-org-mode-map
     (kbd "C-c C-;") 'po:insert)
   (define-key pg-org-mode-map
@@ -17,40 +17,37 @@
   (define-key pg-org-mode-map
     (kbd "C-c C-u") 'po:update-prev)
   (define-key pg-org-mode-map
-    (kbd "C-c C-x") 'po:exit)
-  )
+    (kbd "C-c C-x") 'po:exit))
 
-(defun po:update ()
+(defun po:update-here ()
   (interactive)
-  (let ((cbuf (current-buffer)))
-    (let ((begin 1)
-          (end nil))
-      (unless (org-babel-when-in-src-block) (org-babel-previous-src-block))
-      (setf end (org-babel-where-is-src-block-tail))
-      (goto-char end)
-      (setf po:update-point end)
-      (update-and-eval (current-buffer) begin end))
-    (switch-to-buffer cbuf)))
+  (let ((here (point)))
+    (po:update t)
+    (goto-char here)))
 
 (defun po:update-next ()
   (interactive)
   (goto-char po:update-point)
   (org-babel-next-src-block)
-  (po:update))
+  (po:update t))
 
 (defun po:update-prev ()
   (interactive)
   (goto-char po:update-point)
   (org-babel-previous-src-block)
   (org-babel-previous-src-block)
-  (po:update))
+  (po:update nil))
 
 (defun po:insert ()
   (interactive)
   (when proof-shell-last-output
-    (insert "\n#+BEGIN_EXAMPLE\n")
+    (insert "\n")
+    (org-cycle)
+    (insert "#+BEGIN_EXAMPLE\n")
     (insert (proof-shell-strip-output-markup proof-shell-last-output))
-    (insert "#+END_EXAMPLE\n")))
+    (insert "#+END_EXAMPLE")
+    (org-cycle)
+    (insert "\n")))
 
 (defun po:exit ()
   (interactive)
@@ -75,7 +72,18 @@
         (goto-char (match-end 5))
         (point-at-eol)))))
 
-(defun update-and-eval (buffer &optional begin end)
+(defun po:update (eval-p)
+  (let ((cbuf (current-buffer))
+        (begin 1)
+        (end nil))
+    (unless (org-babel-when-in-src-block) (org-babel-previous-src-block))
+    (setf end (org-babel-where-is-src-block-tail))
+    (goto-char end)
+    (setf po:update-point end)
+    (po:update-and-eval (current-buffer) eval-p begin end)
+    (switch-to-buffer cbuf)))
+
+(defun po:update-and-eval (buffer eval-p begin end)
   (let* ((src-buf buffer)
          (temp-buf (generate-new-buffer "*temp*"))
          (fname (file-name-nondirectory (buffer-file-name src-buf)))
@@ -109,10 +117,9 @@
             (setf pos (abs pos))
             (with-current-buffer c
               (goto-char pos)
-              (delete-region pos (point-max)))
-            (append-to-buffer c pos (point-max))
-            (with-current-buffer c
-              (goto-char (point-max))
-              (proof-goto-point)
-              (setf buffer-offer-save nil)
-              )))))))
+              (delete-region pos (point-max))
+              (setf buffer-offer-save nil))
+            (append-to-buffer c pos (point-max)))))
+      (with-current-buffer c
+        (goto-char (point-max))
+        (when eval-p (proof-goto-point))))))
